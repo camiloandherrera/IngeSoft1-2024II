9ca33cf642155245465a280831b1c14c94458127
Camilo Herrera <cherreragu@unal.edu.co>
2025-02-10 11:46:46 -0500

    docs: update documentation for Clean Code practices and add PyLint/Black setup instructions style: add docstrings to main.py functions gitignore: update .gitignore to include .vscode directory

--- Proyecto/backend/main.py
+++ Proyecto/backend/main.py
@@ -1,11 +1,11 @@
-# Hello world from the dev team!
+"""Hello world from the dev team!"""
+
 from fastapi import FastAPI
 
 app = FastAPI(title="ProjecTrack")
 
+
 @app.get("/")
 async def hello_root():
-    return {
-        "message": "Hello World",
-        "signs": "The ProjectTrack dev team"
-    }
+    """Just a hello world message. :)"""
+    return {"message": "Hello World", "signs": "The ProjectTrack dev team"}
